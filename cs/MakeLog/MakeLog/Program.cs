460d7374faad32279ef3e6422ea5a68f05730f64
Rustam Shakirov <rushchel@gmail.com>
2017-09-17 22:33:54 +0600

    added Nlog, Nlog.Config

--- cs/MakeLog/MakeLog/Program.cs
+++ cs/MakeLog/MakeLog/Program.cs
@@ -3,14 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NLog;
 
 namespace MakeLog
 {
     class Program
     {
+        private static Logger Logger = LogManager.GetCurrentClassLogger();
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            Logger.Trace("Trace message");
             Console.ReadLine();
         }
     }
