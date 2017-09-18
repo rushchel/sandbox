8c4fbefdf9975a8a6a921687d33abedad92e0c39
Rustam Shakirov <rushchel@gmail.com>
2017-09-18 13:31:58 +0600

    added target DataBase for Nlog

--- cs/MakeLog/MakeLog/Program.cs
+++ cs/MakeLog/MakeLog/Program.cs
@@ -15,7 +15,12 @@ namespace MakeLog
         {
             Console.WriteLine("Hello, World!");
             Logger.Trace("Trace message");
+            Logger.Debug("Debug message");
+            Logger.Info("Info message");
+            Logger.Warn("Warn message");
             Logger.Error("Error message");
+            Logger.Fatal("Fatal message");
+            Console.WriteLine("Logs added into table. Press any key to quit.");
             Console.ReadLine();
         }
     }
