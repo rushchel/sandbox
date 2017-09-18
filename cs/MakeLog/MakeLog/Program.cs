a4de547e4c2b84c1ed189e1cad945d4a83e57abb
Rustam Shakirov <rushchel@gmail.com>
2017-09-18 15:38:33 +0600

    Cinfigure log4net with Oracle.ManagedDataAccess

--- cs/MakeLog/MakeLog/Program.cs
+++ cs/MakeLog/MakeLog/Program.cs
@@ -4,22 +4,33 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using NLog;
+using log4net;
 
 namespace MakeLog
 {
     class Program
     {
-        private static Logger Logger = LogManager.GetCurrentClassLogger();
+        private static Logger NLogger = NLog.LogManager.GetCurrentClassLogger();
+        private static readonly ILog LoggerForNet = log4net.LogManager.GetLogger(typeof(Program));
 
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
-            Logger.Trace("Trace message");
-            Logger.Debug("Debug message");
-            Logger.Info("Info message");
-            Logger.Warn("Warn message");
-            Logger.Error("Error message");
-            Logger.Fatal("Fatal message");
+
+            NLogger.Trace("Trace message");
+            NLogger.Debug("Debug message");
+            NLogger.Info("Info message");
+            NLogger.Warn("Warn message");
+            NLogger.Error("Error message");
+            NLogger.Fatal("Fatal message");
+
+            log4net.Config.XmlConfigurator.Configure();
+            LoggerForNet.Debug("Debug message");
+            LoggerForNet.Info("Info message");
+            LoggerForNet.Warn("Warn message");
+            LoggerForNet.Error("Error message");
+            LoggerForNet.Fatal("Fatal message");
+
             Console.WriteLine("Logs added into table. Press any key to quit.");
             Console.ReadLine();
         }
